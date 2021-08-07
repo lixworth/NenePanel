6c836a297856c1ea5a73d009820a3e9cca6a4b48
dhdj <dhdj@cattery.rest>
2021-08-07 14:19:27 +0800

    Initialize database

---
+++ app/Model/Panel/User.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Cattery Mc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author Cattery Team
+ */
+
+namespace App\Model\Panel;
+
+use App\Model\Model;
+
+
+class User extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'user';
+
+    /**
+     * The connection name for the model.
+     *
+     * @var string
+     */
+    protected $connection = 'panel';
+}
--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -10,30 +10,42 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 return [
-    'default' => [
-        'driver' => env('DB_DRIVER', 'mysql'),
-        'host' => env('DB_HOST', 'localhost'),
-        'database' => env('DB_DATABASE', 'hyperf'),
-        'port' => env('DB_PORT', 3306),
-        'username' => env('DB_USERNAME', 'root'),
-        'password' => env('DB_PASSWORD', ''),
-        'charset' => env('DB_CHARSET', 'utf8'),
-        'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
-        'prefix' => env('DB_PREFIX', ''),
+    'panel' => [
+        'driver' => env('PANEL_DB_DRIVER', 'mysql'),
+        'host' => env('PANEL_DB_HOST', 'localhost'),
+        'database' => env('PANEL_DB_DATABASE', 'root'),
+        'port' => env('PANEL_DB_PORT', 3306),
+        'username' => env('PANEL_DB_USERNAME', 'root'),
+        'password' => env('PANEL_DB_PASSWORD', ''),
+        'charset' => env('PANEL_DB_CHARSET', 'utf8'),
+        'collation' => env('PANEL_DB_COLLATION', 'utf8_unicode_ci'),
+        'prefix' => env('PANEL_DB_PREFIX', ''),
         'pool' => [
             'min_connections' => 1,
             'max_connections' => 10,
             'connect_timeout' => 10.0,
             'wait_timeout' => 3.0,
             'heartbeat' => -1,
-            'max_idle_time' => (float) env('DB_MAX_IDLE_TIME', 60),
-        ],
-        'commands' => [
-            'gen:model' => [
-                'path' => 'app/Model',
-                'force_casts' => true,
-                'inheritance' => 'Model',
-            ],
-        ],
+            'max_idle_time' => (float) env('PANEL_DB_MAX_IDLE_TIME', 60),
+        ]
+    ],
+    'daemon' => [
+        'driver' => env('DAEMON_DB_DRIVER', 'mysql'),
+        'host' => env('DAEMON_DB_HOST', 'localhost'),
+        'database' => env('DAEMON_DB_DATABASE', 'root'),
+        'port' => env('DAEMON_DB_PORT', 3306),
+        'username' => env('DAEMON_DB_USERNAME', 'root'),
+        'password' => env('DAEMON_DB_PASSWORD', ''),
+        'charset' => env('DAEMON_DB_CHARSET', 'utf8'),
+        'collation' => env('DAEMON_DB_COLLATION', 'utf8_unicode_ci'),
+        'prefix' => env('DAEMON_DB_PREFIX', ''),
+        'pool' => [
+            'min_connections' => 1,
+            'max_connections' => 10,
+            'connect_timeout' => 10.0,
+            'wait_timeout' => 3.0,
+            'heartbeat' => -1,
+            'max_idle_time' => (float) env('DAEMON_DB_MAX_IDLE_TIME', 60),
+        ]
     ],
 ];
